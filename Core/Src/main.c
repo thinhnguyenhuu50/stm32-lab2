b5fb7d35a7127bc770418710ad8a66bb9a679d56
Thinh <thinh.nguyenhuu50@hcmut.edu.vn>
2025-09-21 17:55:12 +0700

    add the blinking led PA5

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -91,6 +91,7 @@ int main(void)
 	MX_TIM2_Init();
 	/* USER CODE BEGIN 2 */
 	HAL_TIM_Base_Start_IT(&htim2);
+	HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, 1);
 	updateClockBuffer();
 	update7SEG(index_led++);
 	HAL_GPIO_WritePin(DOT_GPIO_Port, DOT_Pin, 0);
@@ -103,6 +104,8 @@ int main(void)
 	while (1)
 	{
 		while (timer0_flag == 0);
+
+		HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
 
 		timer0_flag = 0;
 		++counter;
